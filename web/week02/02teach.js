9b78c6af1d61b3fbea74c4eaab2f53c8ab168b3e
Derek Drennan <derekay@live.com>
2021-01-12 11:36:22 -0800

    finished core reqs

--- web/week02/02teach.js
+++ web/week02/02teach.js
@@ -2,3 +2,15 @@ function buttonClicked()
 {
     alert("Clicked!");
 }
+
+function changeColor()
+{
+    var textbox_id = "txtColor";
+    var textbox = document.getElementById(textbox_id);
+
+    var div_id = "div1";
+    var div = document.getElementById(div_id);
+
+    var color = textbox.nodeValue;
+    div.style.backgroundColor = color;
+}
