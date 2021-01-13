34942fc8ea9cc74b6c0c7060c973f76ca9fb2056
Derek Drennan <derekay@live.com>
2021-01-13 09:59:35 -0800

    working on stretch 1

--- web/week02/02teach.js
+++ web/week02/02teach.js
@@ -3,14 +3,14 @@ function buttonClicked()
     alert("Clicked!");
 }
 
-function changeColor()
-{
-    var textbox_id = "txtColor";
-    var textbox = document.getElementById(textbox_id);
+// function changeColor()
+// {
+//     var textbox_id = "txtColor";
+//     var textbox = document.getElementById(textbox_id);
 
-    var div_id = "div1";
-    var div = document.getElementById(div_id);
+//     var div_id = "div1";
+//     var div = document.getElementById(div_id);
 
-    var color = textbox.value;
-    div.style.backgroundColor = color;
-}
+//     var color = textbox.value;
+//     div.style.backgroundColor = color;
+// }
