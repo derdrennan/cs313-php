5dab47948e274af159f2540855568956b54ce516
Derek Drennan <derekay@live.com>
2021-01-19 17:39:53 -0800

    test

--- web/week03/03teach.php
+++ web/week03/03teach.php
@@ -34,25 +34,25 @@
 
       <!-- Checkboxes -->
       <label for="continents">Continents visited:</label><br>
-      <input type="checkbox" id="NA" name="continents" value="North America">
+      <input type="checkbox" id="NA" name="continents[]" value="North America">
       North America<br>
 
-      <input type="checkbox" id="SA" name="continents" value="South America">
+      <input type="checkbox" id="SA" name="continents[]" value="South America">
       South America<br>
 
-      <input type="checkbox" id="EU" name="continents" value="Europe">
+      <input type="checkbox" id="EU" name="continents[]" value="Europe">
       Europe<br>
 
-      <input type="checkbox" id="AS" name="continents" value="Asia">
+      <input type="checkbox" id="AS" name="continents[]" value="Asia">
       Asia<br>
 
-      <input type="checkbox" id="AU" name="continents" value="Australia">
+      <input type="checkbox" id="AU" name="continents[]" value="Australia">
       Australia<br>
 
-      <input type="checkbox" id="AF" name="continents" value="Africa">
+      <input type="checkbox" id="AF" name="continents[]" value="Africa">
       Africa<br>
-      
-      <input type="checkbox" id="AN" name="continents" value="Antarctica">
+
+      <input type="checkbox" id="AN" name="continents[]" value="Antarctica">
       Antarctica<br>
 
       <!-- Submit Button -->
