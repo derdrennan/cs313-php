38309376e6c5ba125e620d8690a15e33d60ce968
Derek Drennan <derekay@live.com>
2021-01-21 23:47:20 -0800

    starting cart

--- web/week03/03teach.php
+++ web/week03/03teach.php
@@ -15,7 +15,28 @@
       <input type="text" id="email" name="email"><br><br>
 
       <!-- Radio Buttons -->
-      <label for="major">Major:</label><br>
+      <?php
+        $studentsMajor = array(
+          array("CS", "Computer Science"),
+          array("WD", "Web Design"),
+          array("CIT", "Computer Information Technology"),
+          array("CE", "Computer Engineering")
+        );
+      ?>
+
+      <?php
+        for ($row = 0; $row < studentsMajor.count(); $row++) {
+          for ($col = 0; $col < 1; $col++){
+            <input type ="radio" id="studentsMajor[row]" name="major"
+            <?php if (isset($major) && $major=="studentsMajor[row]") echo "checked";?>
+            value="studentsMajor[row][col]">
+            
+          }
+        }
+      ?>
+
+      <!-- Original Radio Buttons -->
+      <!-- <label for="major">Major:</label><br>
       <input type="radio" id="computerScience" name="major" value="Computer Science">
       <label for="computerScience">Computer Science</label><br>
 
@@ -26,34 +47,34 @@
       <label for="CIT">Computer Information Technology</label><br>
 
       <input type="radio" id="CE" name="major" value="Computer Engineering">
-      <label for="CE">Computer Engineering</label><br><br>
+      <label for="CE">Computer Engineering</label><br><br> -->
 
       <!-- Comment box -->
       <label for="userComments">Comments:</label><br>
       <textarea rows="5" cols="50" id="userComments" name="userComments"></textarea><br>
 
-      <!-- Checkboxes -->
+      <!-- Continent Checkboxes -->
       <label for="continents">Continents visited:</label><br>
       <input type="checkbox" id="NA" name="continents[]" value="North America">
       North America<br>
 
       <input type="checkbox" id="SA" name="continents[]" value="South America">
-      South America<br>
+      <label for="SA">South America</label><br>
 
       <input type="checkbox" id="EU" name="continents[]" value="Europe">
-      Europe<br>
+      <label for="EU">Europe</label><br>
 
       <input type="checkbox" id="AS" name="continents[]" value="Asia">
-      Asia<br>
+      <label for="AS">Asia</label><br>
 
       <input type="checkbox" id="AU" name="continents[]" value="Australia">
-      Australia<br>
+      <label for="AU">Australia</label><br>
 
       <input type="checkbox" id="AF" name="continents[]" value="Africa">
-      Africa<br>
+      <label for="AF">Africa</label><br>
 
       <input type="checkbox" id="AN" name="continents[]" value="Antarctica">
-      Antarctica<br>
+      <label for="AN">Antarctica</label><br>
 
       <!-- Submit Button -->
       <input type="submit">
---
+++ web/week03/browse.php
@@ -0,0 +1,100 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>03 Prove</title>
+    <link rel="stylesheet" type="text/css" href="..\week03\03prove.css">
+</head>
+
+<body>
+  <div id="page-container">
+  <header id="header">
+      Pokemon Card Store
+  </header>
+
+  <!-- Store Items -->
+  <div id="card-container">
+  <section class="basic-grid">
+    <div class="assignment-style">
+        Base Set
+        <img src="..\week03\pokemonPics\base-set.jpg" alt="Base Set" width="200" height="278">
+        <div class="add-to-cart">
+            $437
+            <input class="quantity" type="number" id="base" name="base" value="437" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+    <div class="assignment-style">
+        Blastoise
+        <img src="..\week03\pokemonPics\blastoise.jpg" alt="Blastoise" width="200" height="278">
+        <div class="add-to-cart">
+            $457
+            <input class="quantity" type="number" id="blastoise" name="blastoise" value="457" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+    <div class="assignment-style">
+        Charizard
+        <img src="..\week03\pokemonPics\charizard.jpg" alt="Charizard" width="200" height="278">
+        <div class="add-to-cart">
+            $1,750  
+            <input class="quantity" type="number" id="charizard" name="charizard" value="1750" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+    <div class="assignment-style">
+        Fossil Set
+        <img src="..\week03\pokemonPics\fossil-set.jpg" alt="Fossil Set" width="200" height="278">
+        <div class="add-to-cart">
+            $219
+            <input class="quantity" type="number" id="fossil" name="fossil" value="219" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+    <div class="assignment-style">
+        Mew
+        <img src="..\week03\pokemonPics\mew.jpg" alt="Mew" width="200" height="278">
+        <div class="add-to-cart">
+            $199
+            <input class="quantity" type="number" id="mew" name="mew" value="199" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+    <div class="assignment-style">
+        Venusaur
+        <img src="..\week03\pokemonPics\venusaur.jpg" alt="Venusaur" width="200" height="278">
+        <div class="add-to-cart">
+            $187
+            <input class="quantity" type="number" id="venusaur" name="venusaur" value="187" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+    <div class="assignment-style">
+        XY Booster
+        <img src="..\week03\pokemonPics\xy-booster.jpg" alt="XY Booster" width="200" height="278">
+        <div class="add-to-cart">
+            $768
+            <input class="quantity" type="number" id="XY" name="XY" value="768" min="0" max="5" placeholder="0">
+            <button type="button" class="small-text">Add to Cart</button>
+        </div>
+    </div>
+ </section>
+</div>
+
+<?php
+  //Set session variables
+  $_SESSION["cartTotal"] = "$0.00";
+  echo "Cart Total is: " $_SESSION["cartTotal"] . ".";
+  
+
+<footer id="footer">
+  <p>All cards have been certified by PSA Authentication and grading services.</p>
+</footer>
+
+</div>
+</body>
+
+</html>
