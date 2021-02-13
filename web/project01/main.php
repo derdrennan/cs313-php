9f010d05f61b269492605548b8e594fd9c78e3de
Derek Drennan <derekay@live.com>
2021-02-12 22:08:34 -0800

    styling for user form

--- web/project01/main.php
+++ web/project01/main.php
@@ -4,6 +4,7 @@
 
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Project 01: Recipe Sharing</title>
   <link rel="stylesheet" type="text/css" href="../project01/styles.css">
   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Bubblegum+Sans" />
@@ -22,51 +23,53 @@
 <body>
 
   <div id="page-container">
-    <div class="topnav">
-      <a href="../week02/assignmentLinks.html">Home</a>
-      <a class="open-button" onclick="openForm()">Add User</a>
-      <!-- <a href="" onclick="openForm()">Add User</button> -->
-    </div>
+    <div id="content-wrap">
 
-    <div class="form-popup" id="myForm">
-      <form action="insertUser.php" class="form-container" method="POST">
-        <h1>Add New User</h1>
+      <div class="topnav">
+        <a href="../week02/assignmentLinks.html">Home</a>
+        <a class="open-button" onclick="openForm()">Add User</a>
+      </div>
 
-        <label for="username"><b>Username</b></label>
-        <input type="text" id="username" placeholder="Enter Name" name="username" required>
+      <div class="form-popup" id="myForm">
+        <form action="insertUser.php" class="form-container" method="POST">
+          <h1>Add New User</h1>
 
-        <button type="submit" class="btn">Submit</button>
-        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-      </form>
-    </div>
+          <label for="username"><b>Username</b></label>
+          <input type="text" id="username" placeholder="Enter Name" name="username" required>
 
-    <h1 id="header-1">Family Recipe Sharing</h1><br>
+          <button type="submit" class="btn">Submit</button>
+          <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+      </div>
 
-    <div id="wrapper">
-      <div id="intro">
-        Welcome to my "Recipe Sharing" website! I live so far from family that I miss
-        not being able to have family over for dinner. One of my favorite things is
-        experiencing new meals and seeing what other people like to eat. This is a way
-        I set up for us to share our favorite recipes with each other so we can make them
-        and bond through food, even if it's from different parts of the country.<br><br>
-        When this site is finished, you will be able to add yourself as a member, and then
-        add your favorite recipes.
-      </div><br>
+      <h1 id="header-1">Family Recipe Sharing</h1><br>
 
-      <div id="card-container">
-        <section class="basic-grid">
-          <?php
-          $db = get_db();
-          foreach ($db->query('SELECT id, username FROM public.user') as $row) {
-          ?>
-            <a href="../project01/recipeList.php?user=<?php echo $row['id'] ?>" class="assignment-style link-style">
-              <?php echo $row['username']; ?>
-            </a>
-          <?php
-          }
-          ?>
+      <div id="wrapper">
+        <div id="intro">
+          Welcome to my "Recipe Sharing" website! I live so far from family that I miss
+          not being able to have family over for dinner. One of my favorite things is
+          experiencing new meals and seeing what other people like to eat. This is a way
+          I set up for us to share our favorite recipes with each other so we can make them
+          and bond through food, even if it's from different parts of the country.<br><br>
+          When this site is finished, you will be able to add yourself as a member, and then
+          add your favorite recipes.
+        </div><br>
 
-        </section>
+        <div id="card-container">
+          <section class="basic-grid">
+            <?php
+            $db = get_db();
+            foreach ($db->query('SELECT id, username FROM public.user') as $row) {
+            ?>
+              <a href="../project01/recipeList.php?user=<?php echo $row['id'] ?>" class="assignment-style link-style">
+                <?php echo $row['username']; ?>
+              </a>
+            <?php
+            }
+            ?>
+
+          </section>
+        </div>
       </div>
     </div>
 
