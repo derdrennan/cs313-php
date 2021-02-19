54e60587fdaabe8851bb6591d1a8019af27d5f4f
Derek Drennan <derekay@live.com>
2021-02-19 07:28:37 -0800

    debug login

--- web/project01/login.php
+++ web/project01/login.php
@@ -21,8 +21,10 @@ echo "<br>";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   // Values for queries
-  $email = test_input($_POST['emailLI']);
-  $password = test_input($_POST['passwordLI']);
+  // $email = test_input($_POST['emailLI']);
+  // $password = test_input($_POST['passwordLI']);
+  $email = $_POST['emailLI'];
+  $password = $_POST['passwordLI'];
 
   //For debugging
   echo "E-mail: " . $email . "<br>";
